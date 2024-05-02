f9c2ad5a769cb834dd44c50ab2031fe0e78ea270
zxwgm <jeff0630@naver.com>
2024-05-02 16:43:34 +0900

    map

--- src/client/js/course.js
+++ src/client/js/course.js
@@ -5,8 +5,41 @@ let userLatitude;
 let userLongitude;
 let isMapDrawn = false; //bool
 let courseData = [];
-let markers = [];
 // 마커를 그리는 함수
+let markers = [];
+let clickCourse = 0; // 0 내자신으로, 나머진 id
+
+const panTo = (latitude, longitude) => {
+  const moveLatLon = new kakao.maps.LatLng(latitude, longitude);
+  map.panTo(moveLatLon);
+};
+
+const clickCourseList = (e, courseNo) => {
+  if (clickCourse !== courseNo) {
+    const courseWrap = document.querySelectorAll(".course");
+    for (let i = 0; i < courseWrap.length; i++) {
+      courseWrap[i].classList.remove("on");
+    }
+    // 클릭한 애 색칠
+    e.currentTarget.classList.add("on");
+    // 더해야함
+
+    let courseLatitude;
+    let course_longitude;
+
+    if (courseNo === 0) {
+      courseLatitude = userLatitude;
+      course_longitude = userLongitude;
+    } else {
+      const matchCourse = courseData.find((c) => c.course_no === courseNo);
+      courseLatitude = matchCourse.course_latitude;
+      course_longitude = matchCourse.course_longitude;
+    }
+    panTo(courseLatitude, course_longitude);
+    clickCourse = courseNo;
+  }
+};
+
 const addMarker = (position) => {
   let marker = new kakao.maps.Marker({
     position: position,
@@ -80,6 +113,9 @@ const configLocation = () => {
         isMapDrawn = true;
       }
       addMarker(new kakao.maps.LatLng(userLatitude, userLongitude));
+      if (clickCourse === 0) {
+        panTo(userLatitude, userLongitude);
+      }
     });
   }
 };
@@ -88,11 +124,11 @@ const makeCourseNaviHTML = (data) => {
   const courseWrap = document.getElementById("courseWrap");
   let html = "";
   for (let i = 0; i < data.length; i++) {
-    html += `<li class="course">`;
+    html += `<li class="course" onclick="clickCourseList(event, ${data[i].course_no})">`;
     html += `<p>${data[i].course_name}</p>`;
     html += `</li>`;
   }
-  html += `<li id = "myPosition" class="course on"> 나의위치</li>`;
+  html += `<li id = "myPosition" class="course on"  onclick="clickCourseList(event, 0)"> 나의위치</li>`;
 
   courseWrap.innerHTML = html;
 };
